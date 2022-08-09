d1dc677c7a536ee0ee66f962b937c61775c303c9
root <root@DESKTOP-K0KHTBP.localdomain>
2022-08-08 22:23:06 -0500

    add create child shell function

---
+++ shell_create_child.c
@@ -0,0 +1,33 @@
+#include "shell.h"
+/**
+ * create_child - C - Simple Shell
+ * @_line : received buffer
+ * @_args : buffer arguments
+ * @_av : argument vector
+ * Description: signal Ctrl + D
+ * Return: nothing
+ */
+int create_child(char *_line, char **_args, char **_av)
+{
+	pid_t parent = 0;
+	int status = 0;
+
+	parent = fork();
+	if (!parent)
+	{
+		if (execve(_args[0], _args, NULL) == -1)
+		{
+			printf("%s: No such file or directory\n", _av[0]);
+			free_arg(_args);
+			free(_line);
+			return (1);
+		}
+	}
+	else
+	{
+		wait(&status);
+	}
+	free_arg(_args);
+	free(_line);
+	return (0);
+}
