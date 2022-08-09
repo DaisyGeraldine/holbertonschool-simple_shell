58e4e68d73a93cef8240effa4f20e9f5a8d085b2
DaisyGeraldine <chipanal@hotmail.com>
2022-08-09 11:22:35 -0500

    test status output

--- shell_create_child.c
+++ shell_create_child.c
@@ -9,25 +9,36 @@
  */
 int create_child(char *_line, char **_args, char **_av)
 {
-	pid_t parent = 0;
+	pid_t parent;
 	int status = 0;
-
+	char  wpid;
+	(void)wpid;
 	parent = fork();
-	if (!parent)
+
+	if (parent == 0)
 	{
 		if (execve(_args[0], _args, NULL) == -1)
 		{
-			printf("%s: No such file or directory", _av[0]);
+			printf("%s: No such file or directory\n", _av[0]);
 			free_arg(_args);
 			free(_line);
-			return (1);
+			return (0);
 		}
+		exit(EXIT_FAILURE);
+	}
+	else if (parent < 0)
+	{
+		printf("%s: No such file or directory\n", _av[0]);
+		return (0);
 	}
 	else
 	{
-		wait(&status);
+		/*wait(&status);*/
+		do {
+			wpid = waitpid(parent, &status, WUNTRACED);
+		} while (!WIFEXITED(status) && !WIFSIGNALED(status));
 	}
 	free_arg(_args);
 	free(_line);
-	return (0);
+	return (1);
 }
